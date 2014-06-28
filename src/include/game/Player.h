f09a4c8ebcb043ebdaec6ccadb2035fd0ed89237
Arthur HUGEAT <hugeat.arthur@gmail.com>
2014-06-28 18:30:41 +0200

    Update the moving system.

--- src/include/game/Player.h
+++ src/include/game/Player.h
@@ -6,21 +6,13 @@
 #include <game/Element.h>
 
 namespace game {
-  
-  enum class PlayerMove{
-    UP,
-    LEFT,
-    BOTTOM,
-    RIGHT,
-  };
 
   class Player : public Element
   {
   public:
     explicit Player(ElementType type, float x, float y, b2World *world);
     
-    void move(PlayerMove move);
-    void stop(PlayerMove move);
+    void move(float vx, float vy);
 
   private:
 
