db65b97dd46d8b28b7029f49ff38f606b8e4df32
Anne Héam <anne@yuffie>
2014-06-28 16:00:43 +0200

    change color, shape circle

--- src/lib/Element.cc
+++ src/lib/Element.cc
@@ -13,11 +13,11 @@ namespace game {
 
     m_body = world->CreateBody(&def);
 
-    b2PolygonShape box;
-    box.SetAsBox(1.0f, 1.0f);
+    b2CircleShape circle;
+    circle.m_radius = 10.0f;
 
     b2FixtureDef fixture;
-    fixture.shape = &box;
+    fixture.shape = &circle;
     fixture.density = 1.0f;
     fixture.friction = 0.3f;
 
@@ -36,8 +36,19 @@ namespace game {
     sf::CircleShape shape(10.0f);
     shape.setOrigin(10.0f, 10.0f);
     shape.setPosition(pos.x, pos.y);
-    shape.setFillColor(sf::Color::Red);
-
+    switch(type){
+    case(PAPER): 
+      shape.setFillColor(sf::Color::Red);
+      break;
+    case(ROCK):
+      shape.setFillColor(sf::Color::Green);
+      break;
+    case(SCISSORS):
+      shape.setFillColor(sf::Color::Blue);
+      break;
+    default : 
+      shape.setFillColor(sf::Color::Yellow);
+    }
     window.draw(shape);
   }
 
